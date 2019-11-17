e28c1e4e8a593a6a8f48b685ebccad8de135cf3f
starcraft.patrick@gmail.com <phufnagl@Stefans-MBP.fritz.box>
2019-11-17 17:01:24 +0100

    tabs complete

--- www/js/Controller.js
+++ www/js/Controller.js
@@ -5,7 +5,7 @@ var Controller = function () {
         initialize: function () {
             self = this;
             this.bindEvents();
-            //self.renderSettingsView();
+            self.renderFolderView();
             var btnw = $("#cog").height();
             $('#cog').css({'width':btnw+'px'});
             var tabImg = $(".tabPng").height();
@@ -18,14 +18,37 @@ var Controller = function () {
             document.getElementById("cog").addEventListener("click", function () {
                 self.renderSettingsView();
             });
+            $('.tab').on('click', this.onTabClick);
         },
 
 
         onTabClick: function (e) {
-
+            e.preventDefault();
+            if ($(this).hasClass('active')) {
+                return;
+            }
+            
+            var tab = $(this).context.id;
+            if (tab === 'folder') {
+                self.renderFolderView();
+            }
+            if (tab === 'sanduhr'){
+                self.renderSanduhrView();
+            }
+            if (tab === 'group'){
+                self.renderGroupView();
+            }
+            if (tab === 'stat'){
+                self.renderStatView();
+            }   
         },
 
         renderSettingsView: function () {
+
+            $("#folder .tabPng").removeClass('active');
+            $("#sanduhr .tabPng").removeClass('active');
+            $("#group .tabPng").removeClass('active');
+            $("#stat .tabPng").removeClass('active');
 
             var $tab = $('#pageContent div');
             $tab.empty();
@@ -61,7 +84,54 @@ var Controller = function () {
                     self.renderSettingsView();
                 }
             });
-        }
+        },
+        renderFolderView: function(){
+
+            $("#folder .tabPng").addClass('active');
+            $("#sanduhr .tabPng").removeClass('active');
+            $("#group .tabPng").removeClass('active');
+            $("#stat .tabPng").removeClass('active');
+
+            var $tab = $('#pageContent div');
+            $tab.empty();
+            $("#pageContent").load("./views/folder.html", function (data) {});
+        },
+
+        renderSanduhrView: function(){
+
+            $("#sanduhr .tabPng").addClass('active');
+            $("#folder .tabPng").removeClass('active');
+            $("#group .tabPng").removeClass('active');
+            $("#stat .tabPng").removeClass('active');
+
+            var $tab = $('#pageContent div');
+            $tab.empty();
+            $("#pageContent").load("./views/sanduhr.html", function (data) {});
+        },
+
+        renderGroupView: function(){
+
+            $("#group .tabPng").addClass('active');
+            $("#sanduhr .tabPng").removeClass('active');
+            $("#folder .tabPng").removeClass('active');
+            $("#stat .tabPng").removeClass('active');
+
+            var $tab = $('#pageContent div');
+            $tab.empty();
+            $("#pageContent").load("./views/group.html", function (data) {});
+        },
+
+        renderStatView: function(){
+
+            $("#stat .tabPng").addClass('active');
+            $("#sanduhr .tabPng").removeClass('active');
+            $("#group .tabPng").removeClass('active');
+            $("#folder .tabPng").removeClass('active');
+
+            var $tab = $('#pageContent div');
+            $tab.empty();
+            $("#pageContent").load("./views/stat.html", function (data) {});
+        },
 
     }
     controller.initialize();
